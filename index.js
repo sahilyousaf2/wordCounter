42f177b0fa50ee91531daa85fa5801eb056b3615
Sahil Yousaf <160120573+sahilyousaf2@users.noreply.github.com>
2024-04-17 23:10:41 +0500

    first

---
+++ index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+//Import 'Inquirer' and 'Chalk' modules, which is a command line interface for Node.js
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+console.log(chalk.magentaBright.bold('\n \t "Welcome To codeWithSahil - Word Counter Application"'));
+let condition = true;
+while (condition) {
+    //Declare a const 'answer' and assign it to the result of inquirer.prompt function
+    const answers = await inquirer.prompt([
+        {
+            name: 'Sentence',
+            type: 'input',
+            message: chalk.greenBright.bold('Enter Your Sentence To Count The Word :')
+        }
+    ]);
+    let words = answers.Sentence.trim().split(' ');
+    //print the array of words to the console
+    console.log(words);
+    //Print the count to the console
+    console.log(chalk.green(`\n \t Your Sentence Words Count Is : ${words.length}`));
+    let againAns = await inquirer.prompt([
+        {
+            name: 'again',
+            type: 'confirm',
+            message: chalk.greenBright.bold('Do You Want To Count Again ?'),
+            default: false
+        }
+    ]);
+    condition = againAns.again;
+}
+console.log(chalk.grey.bold('\n \t Thanks You'));
